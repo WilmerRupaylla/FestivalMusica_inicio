a56d1c2e5f039122aeb6112359fd287eacd8dae2
Wilmer Rupaylla <wilmeradalidrm@outlook.com>
2022-07-24 17:17:05 -0400

    24-07-2022

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const plumber = require("gulp-plumber");
 const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
+const avif =require("gulp-avif")
 
 function css(done) {
   // Idenfica l arcivo de sass
@@ -22,11 +23,11 @@ function css(done) {
 
 function imagenes(done) {
   const opciones = {
-    optimizationLeve:3
-  }
+    optimizationLeve: 3,
+  };
   src("src/img/**/*.{png,jpg}")
     .pipe(cache(imagemin(opciones)))
-  .pipe(dest("build/img"))
+    .pipe(dest("build/img"));
   done();
 }
 function versionWebp(done) {
@@ -37,6 +38,15 @@ function versionWebp(done) {
 
   done();
 }
+
+function versionavif(done) {
+  const opciones = {
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}").pipe(avif(opciones)).pipe(dest("build/img"));
+
+  done();
+}
 function dev(done) {
   watch("src/scss/**/*.scss", css);
   done();
@@ -44,4 +54,5 @@ function dev(done) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes,versionWebp, dev);
+exports.versionavif = versionavif;
+exports.dev = parallel(imagenes,versionavif, versionWebp, dev);
