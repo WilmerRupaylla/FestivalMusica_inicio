520b013aaf550d3748f57fba84cd903dc0514a95
Wilmer Rupaylla <wilmeradalidrm@outlook.com>
2022-07-26 20:06:05 -0400

    26-07-2022

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,27 @@ const { src, dest, watch, parallel } = require("gulp");
 // CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps =require('gulp-sourcemaps')
 
 // IMAGENES
 const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
-const avif =require("gulp-avif")
+const avif = require("gulp-avif");
 
 function css(done) {
   // Idenfica l arcivo de sass
   src("src/scss/**/*.scss")
+  .pipe(sourcemaps.init())
     .pipe(plumber())
     // Compilarlo
     .pipe(sass())
     // Almacenao en el disco duro
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest("build/css"));
   done();
 }
@@ -47,12 +54,22 @@ function versionavif(done) {
 
   done();
 }
-function dev(done) {
-  watch("src/scss/**/*.scss", css);
+
+function javascript(done) {
+  src("src/js/**/*.js").pipe(dest("build/js"));
   done();
 }
+
+function dev(done) {
+  watch("src/scss/**/*.scss", css);
+  const dep = 7;
+  watch("src/js/**/*.js", javascript);
+  done();
+}
+
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionavif = versionavif;
-exports.dev = parallel(imagenes,versionavif, versionWebp, dev);
+exports.dev = parallel(imagenes, versionavif, versionWebp, javascript, dev);
